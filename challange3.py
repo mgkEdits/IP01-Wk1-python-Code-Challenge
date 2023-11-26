4c051053d8047e35e23e524a35e1161ca14c03bf
mgkEdits <elijahnyasiando.mn@gmail.com>
2023-11-26 20:54:33 +0300

    challange 3

---
+++ challange3.py
@@ -0,0 +1,24 @@
+# Challenge 3: Consonant value
+def consonant_value(word):
+    consonant_values = {letter: ord(letter) - ord('a') + 1 for letter in word if letter not in "aeiou"}
+    
+    # Group consecutive consonants
+    groups = []
+    current_group = []
+    for letter in word:
+        if letter not in "aeiou":
+            current_group.append(letter)
+        else:
+            if current_group:
+                groups.append(current_group)
+                current_group = []
+    if current_group:
+        groups.append(current_group)
+    
+    # Calculate values for each group and return the maximum
+    max_value = 0
+    for group in groups:
+        group_value = sum(consonant_values[letter] for letter in group)
+        max_value = max(max_value, group_value)
+    
+    return max_value
