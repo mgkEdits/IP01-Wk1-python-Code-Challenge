d47c599e2fd54958c17244eb0aa0f3d11b181dc2
mgkEdits <elijahnyasiando.mn@gmail.com>
2023-11-27 09:09:39 +0300

    test cases

--- challange2.py
+++ challange2.py
@@ -8,3 +8,11 @@ def is_two_positive(a, b, c):
     if c > 0:
         positive_count += 1
     return positive_count == 2
+
+# Test cases
+print(is_two_positive(2, 4, -3))  # Output: True
+print(is_two_positive(-4, 6, 8))  # Output: True
+print(is_two_positive(4, -6, 9))  # Output: True
+print(is_two_positive(-4, 6, 0))  # Output: False
+print(is_two_positive(4, 6, 10))  # Output: False
+print(is_two_positive(-14, -3, -4))  # Output: False
