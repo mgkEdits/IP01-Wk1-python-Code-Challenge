e2f0f0b5fd0516eedead7176096ac48f2eefefda
mgkEdits <elijahnyasiando.mn@gmail.com>
2023-11-27 10:08:15 +0300

    error handling in time

--- challange1.py
+++ challange1.py
@@ -1,7 +1,14 @@
 # Challenge 1: Converting 12-hour time to 24-hour time
-def convert_to_24_hour(hour, minute, period):
+def convert_to_24_hour(hour,minute, period):
+     
+    if minute >59 or hour >12:
+        return("Error in time, please try again")
+
     if period.lower() == "pm" and hour < 12:
         hour += 12
     elif period.lower() == "am" and hour == 12:
         hour = 0
     return f"{hour:02d}{minute:02d}"
+
+# Test cases
+print(convert_to_24_hour(13,50, "am"))  
